34c41629178d46e786f8c612733e54c863d09f1d
nkolson-cyber <nkolson@csuchico.edu>
2026-08-31 11:39:03 -0700

    fixed divide by zero error, close #61

--- main.cpp
+++ main.cpp
@@ -16,8 +16,13 @@ int main()
     cout << "Addition: " << x + y << endl;
     cout << "Subtraction: " << x - y << endl;
     cout << "Multiplication: " << x * y << endl;
-    cout << "Division: " << x / y << endl;
-    cout << "Remainder: " << x % y << endl;
+    if (y != 0) {
+        cout << "Division: " << x / y << endl;
+        cout << "Remainder: " << x % y << endl;
+
+    } else {
+        cout << "Dividing by zero is not a number." << endl;
+    }
     cout << "Square Root: " << sqrt(x) << endl;
     cout << "Square: " << pow(x, y) << endl;
 
